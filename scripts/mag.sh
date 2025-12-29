3c872d5f63fd5d3b49074b081ffeefdbc03d2a8c
ystone1101 <97172939+ystone1101@users.noreply.github.com>
2025-12-29 13:52:41 +0900

    /////

--- scripts/mag.sh
+++ scripts/mag.sh
@@ -544,11 +544,9 @@ for R1_QC_GZ in "${QC_READS_DIR}"/*_1.fastq.gz; do
     SAMPLE_BASE=$(basename "$R1_QC_GZ")
         
     # 2. R2 파일 경로 추론 (샘플명 + _2.fastq.gz)
-    R2_BASE="${SAMPLE_BASE/_1.fastq.gz/_2.fastq.gz}"
     R2_QC_GZ="${QC_READS_DIR}/${R2_BASE}"
         
     # 2. kneaddata, fastp 등 불필요한 꼬리표 제거 (깔끔한 이름 생성)
-    # SAMPLE=$(echo "$SAMPLE_BASE" | sed -E 's/(_1|_2|_R1|_R2)?(_kneaddata|_paired|_unpaired|_fastp)?\.fastq\.gz//g')
     SAMPLE=$(echo "$SAMPLE_BASE" | sed -E 's/(_1|_2|_R1|_R2|_kneaddata|_paired|_unpaired|_fastp).*//')
 
     # 3. R2 파일 존재 여부 확인
